0971b418fa9e81dbd0078cbe0a0be752812bd423
nakedi <nakedilets@gmail.com>
2019-03-19 15:59:15 +0200

    added more functions

--- mypackage/sorting.py
+++ mypackage/sorting.py
@@ -7,10 +7,114 @@ def bubble_sort(items):
     Returns:
         array : array of items, sorted in ascending order
     Example:
-        >>> bubble_sort([20,15,3,])
+        >>> bubble_sort([20,15,3])
         [3,15,20]
     """
 
     """
     docstring goes here
     """
+
+    for item in range(len(items)-1,0,-1):
+        for i in range(item):
+            if items[i]>items[i+1]:
+                temp = items[i]
+                items[i] = items[i+1]
+                items[i+1] = temp
+    return items
+
+
+def merge_sort(items):
+
+        """Return array of items, sorted in ascending order
+        Args:
+            items: list or array-like object containing numerical values.
+
+        Returns:
+            array : array of items, sorted in ascending order
+        Example:
+            >>> merge_sort([50,10,5])
+            [5,10,50]
+        """
+
+        """
+        docstring goes here
+        """
+        if len(items)>1:
+        mid = len(items)//2
+        lefthalf = items[:mid]
+        righthalf = items[mid:]
+
+        merge_sort(lefthalf)
+        merge_sort(righthalf)
+
+        i=0
+        j=0
+        k=0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                items[k]=lefthalf[i]
+                i=i+1
+            else:
+                items[k]=righthalf[j]
+                j=j+1
+            k=k+1
+
+        while i < len(lefthalf):
+            items[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+
+        while j < len(righthalf):
+            items[k]=righthalf[j]
+            j=j+1
+            k=k+1
+
+    return items
+
+
+
+
+
+def quick_sort(items):
+    """Return array of items, sorted in ascending order
+    Args:
+        items: list or array-like object containing numerical values.
+
+    Returns:
+        array : array of items, sorted in ascending order
+    Example:
+        >>> quick_sort([50,100,555,11])
+        [11,50,100,555]
+    """
+
+    """
+    docstring goes here
+    """
+   if len(items) == 2:
+       if items[0] > items[1]:
+           return [items[1], items[0]]
+       else:
+           return items
+   else:
+       pivot_val = items[0]
+       left_list = []
+       right_list = []
+       equal_list = []
+
+       for item in range(len(items)):
+           if items[item] == pivot_val:
+               equal_list.append(items[item])
+           elif items[item] < pivot_val:
+               left_list.append(items[item])
+           else:
+               right_list.append(items[item])
+
+       if len(left_list) > 1:
+           left_list = quick_sort(left_list)
+       if len(right_list) > 1:
+           right_list = quick_sort(right_list)
+
+       sorted_list = left_list + equal_list + right_list
+
+   return sorted_list
--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,6 @@
 from  mypackage import recursion, sorting
+
+
 def sum_array(array):
     """
     make sure sum_array works correctly
@@ -38,4 +40,27 @@ def reverse(word):
 
     assert recursion.reverse(nakedi) == idekan, 'incorrect'
     assert recursion.reverse(evol) ==love, 'incorrect'
-    assert recursion.factorial(madam) == madam, 'incorrect'
+    assert recursion.reverse(madam) == madam, 'incorrect'
+
+
+
+def bubble_sort(items):
+    assert sorting.bubble_sort([50,30,40,10,20]) == [10,20,30,40,50], 'incorrect'
+    assert sorting.bubble_sort([9,8,7]) == [7,8,9], 'incorrect'
+    assert sorting.bubble_sort([10,4,2,0]) == [0,2,4,10], 'incorrect'
+
+
+
+
+def merge_sort(items):
+    assert sorting.merge_sort([77,31,45,55,10]) == [10, 31,45,55, 77, 93], 'incorrect'
+    assert sorting.merge_sort([3, 1, 2]) == [1,2,3], 'incorrect'
+    assert sorting.merge_sort([5,12,78,1,2,6]) == [1,2,5,6,12,78], 'incorrect'
+
+
+
+
+def quick_sort(items):
+    assert sorting.quick_sort([77,31,44,55,20]) == [31, 44, 54, 55, 77], 'incorrect'
+    assert sorting.quick_sort([31, 21, 23]) == [21,23,31], 'incorrect'
+    assert sorting.quick_sort([5,12,78,1]) == [1,5,12,78], 'incorrect'
