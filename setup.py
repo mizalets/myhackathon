b1604e9cc0f46c3585109f747028aa030d8c6f69
nakedi <nakedilets@gmail.com>
2019-03-19 16:53:07 +0200

    This will work

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description='EDSA example python package',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
-    url='https://github.com/mizalets/myhackathon',
+    url='https://github.com/mizalets/myhackathon/',
     author='Nakedi Letsoalo',
     author_email='nakedilets@gmail.com'
 )
