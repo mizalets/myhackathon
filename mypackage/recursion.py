ef44735b98b130a0e4db1db944f40d8e3811dc12
nakedi <nakedilets@gmail.com>
2019-03-19 16:21:38 +0200

    Changes

--- mypackage/recursion.py
+++ mypackage/recursion.py
@@ -66,10 +66,10 @@ def factorial(n):
     """
     docstring goes here
     """
-         num = 1
-         for i in range(1, n + 1):
-             num *= i
-         return num
+     num = 1
+     for i in range(1, n + 1):
+         num *= i
+     return num
 
 
 
