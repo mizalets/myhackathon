5c39b65cf40bd1614b341ef750da440bf72b60a7
nakedi <nakedilets@gmail.com>
2019-03-19 15:10:13 +0200

    Initial commit

---
+++ mypackage/sorting.py
@@ -0,0 +1,16 @@
+def bubble_sort(items):
+
+    """Return array of items, sorted in ascending order
+    Args:
+        items: list or array-like object containing numerical values.
+
+    Returns:
+        array : array of items, sorted in ascending order
+    Example:
+        >>> bubble_sort([20,15,3,])
+        [3,15,20]
+    """
+
+    """
+    docstring goes here
+    """
